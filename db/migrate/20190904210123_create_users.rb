aecd1ccac54fe6afc7f1ddb7c00827110aa6430d
Oscar Bellechasse <obellech10@gmail.com>
2019-09-04 15:08:20 -0600

    Create migrations for user, events & create event tables

---
+++ db/migrate/20190904210123_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.integer :phone
+      t.string :email
+    end
+  end
+end
---
+++ db/migrate/20190904210157_create_event_attendees.rb
@@ -0,0 +1,9 @@
+class CreateEventAttendees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :event_attendees do |t|
+      t.references :user, foreign_key: true
+      t.references :event, foreign_key: true
+      t.integer :rsvp
+    end
+  end
+end
