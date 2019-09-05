0cfb0b63608910ca06a8d137795249ff09d9f886
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-05 16:11:17 -0600

    Changes spacing

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -2,6 +2,10 @@ class Credential < ApplicationRecord
   belongs_to :user
 
   def self.find_or_create_by_omniauth(auth)
-    find_or_create_by(uid: auth["uid"], provider: auth["provider"], token: auth["credentials"]["token"])
+    find_or_create_by(
+      uid: auth["uid"], 
+      provider: auth["provider"], 
+      token: auth["credentials"]["token"]
+      )
   end
 end
