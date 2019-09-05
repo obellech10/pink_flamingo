ca343f17b6a97523258bf7b0ab2babeae7efef56
Oscar Bellechasse <obellech10@gmail.com>
2019-09-05 17:20:03 -0600

    Update profile route and about routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get '/about', to: 'welcome#about', as: :about
+  get '/about', to: 'about#show', as: :about
   get '/login', to: "sessions#new", as: :login
 
   get '/auth/google', as: :google_login
   get '/auth/facebook', as: :facebook_login
 
-  resources :profile, only: [:show]
+  get '/profile', to: 'users#show', as: :profile
   resources :events, only: [:index, :show]
 
   get 'auth/facebook/callback', to: 'facebook/sessions#create'
