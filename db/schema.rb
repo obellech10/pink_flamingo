07e51127f2cd63d985c876360635f7017a149d3f
Jori Peterson <joripeterson@gmail.com>
2019-09-04 21:39:34 -0600

    Add columns to user for facebook oauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_210157) do
+ActiveRecord::Schema.define(version: 2019_09_05_025624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2019_09_04_210157) do
     t.integer "zip"
     t.integer "phone"
     t.string "email"
+    t.string "provider"
+    t.string "uid"
   end
 
   add_foreign_key "event_attendees", "events"
