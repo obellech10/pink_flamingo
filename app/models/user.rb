dd65e5a62ed410cbd7c1473117b556bd6e6cdfec
Jori Peterson <joripeterson@gmail.com>
2019-09-05 15:45:57 -0600

    Comment out validations for users (for now)
    Co-Authored-By: Mills Provosty <millsprovosty@gmailcom>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,17 @@
 class User < ApplicationRecord
-  validates_presence_of :first_name
-  validates_presence_of :last_name
-  validates_presence_of :address
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :zip
-  validates_presence_of :email
+  # validates_presence_of :first_name
+  # validates_presence_of :last_name
+  # validates_presence_of :address
+  # validates_presence_of :city
+  # validates_presence_of :state
+  # validates_presence_of :zip
+  # validates_presence_of :email
 
   has_many :events
   has_many :event_attendees
+  has_many :credentials
 
   def self.create_with_omniauth(auth)
-    user = find_or_create_by(uid: auth["uid"], provider:  auth["provider"])
-    user.email = "#{auth["uid"]}@#{auth["provider"]}.com"
-    user.password = auth["uid"]
-    user.name = auth["info"]["name"]
-    if User.exists?(user)
-      user
-    else
-    user.save!
-    user
-    end
+    create(first_name: auth[:extra][:raw_info][:name])
   end
 end
