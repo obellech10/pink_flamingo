d437c2920c0a3e923d1f5155fd4ca0f271d3e472
Oscar Bellechasse <obellech10@gmail.com>
2019-09-05 16:31:38 -0600

    Removed the validations for type, food and booze as this was causing errors. Type is a reserved word and when entering in false for food and booze creation of the event was not allowed

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,9 +6,7 @@ RSpec.describe Event, type: :model do
     it {should validate_presence_of(:time)}
     it {should validate_presence_of(:address)}
     it {should validate_presence_of(:user_id)}
-    it {should validate_presence_of(:type)}
-    it {should validate_presence_of(:food)}
-    it {should validate_presence_of(:booze)}
+    # it {should validate_presence_of(:type)}
   end
 
 	describe 'relationships' do
