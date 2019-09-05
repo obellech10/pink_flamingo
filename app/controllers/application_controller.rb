5d336a95b2215e11498648eff7afda8770adda06
Jori Peterson <joripeterson@gmail.com>
2019-09-05 15:44:32 -0600

    Set up helper methods in application controller
    Co-Authored-By: Mills Provosty <millsprovosty@gmailcom>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  helper_method :signed_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def signed_in?
+    !!current_user
+  end
+ 
 end
