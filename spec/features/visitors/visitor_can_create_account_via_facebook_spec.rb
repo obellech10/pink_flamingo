3097839c05713c33bb07d6ee20ee894236b730cf
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-06 08:56:42 -0600

    Update visitor_can_create_account_via_facebook_spec.rb

--- spec/features/visitors/visitor_can_create_account_via_facebook_spec.rb
+++ spec/features/visitors/visitor_can_create_account_via_facebook_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe 'User login' do
-	it 'can sign in using Facebook'do
-		stub_omniauth
+  it 'can sign in using Facebook'do
+    stub_omniauth
 
     visit '/'
     click_on "Login"
 
     expect(current_path).to eq(login_path)
 
-		click_on "Connect via Facebook"
+    click_on "Connect via Facebook"
 
     expect(current_path).to eq(profile_path)
-		expect(page).to_not have_content("Connect via Facebook")
+    expect(page).to_not have_content("Connect via Facebook")
   end
 end
