cd32f7e63ad95f404188d7fbb3f2671b0d50ad34
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-05 18:20:20 -0600

    Begins test

---
+++ spec/features/user/user_profile_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "As a logged in User on my profile page" do
+  describe "I can see fields to fill in my personal info" do
+    it "When I hit submit I am rerouted to my profile page where I see that info" do
+
+      
+    end
+  end
+end
