e5b14a392489ff89d9b468b8dd6f7cdeeab15e47
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-05 18:25:09 -0600

    Adds factory for User

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Dog.name }
+    last_name { Faker::Artist.name }
+    address { Faker::Providers.address}
+    email { Faker::Internet.email }
+    city { Faker::Providers.city }
+    state { Faker::Providers.state }
+    phone { Faker::Providers.phone_number }
+    email { Faker::Internet.safe_email }
+  end
+end
