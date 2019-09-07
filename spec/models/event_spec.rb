9900dbf7b4a9bd9e353df714ea07eac14d3c5757
Jori Peterson <joripeterson@gmail.com>
2019-09-07 12:15:01 -0600

    Delete "type" validation from event_spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Event, type: :model do
     it {should validate_presence_of(:time)}
     it {should validate_presence_of(:address)}
     it {should validate_presence_of(:user_id)}
-    # it {should validate_presence_of(:type)}
   end
 
 	describe 'relationships' do
