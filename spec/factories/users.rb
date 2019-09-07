fa939953b5ff79d7a21ae4c5058ff7e570233e38
Jori Peterson <joripeterson@gmail.com>
2019-09-07 14:14:17 -0600

    Update factory bot for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,23 +1,11 @@
 FactoryBot.define do
   factory :user do
-<<<<<<< HEAD
-    first_name { 'Baxter' }
-    last_name { 'McDog' }
+    name { 'Baxter McDog' }
     address { "1234 Hello Ave"}
     city { "Denver" }
     state { "CO" }
     zip { 80802 }
     phone { "415-999-0987" }
     email { "baxter@gmail.com" }
-=======
-    name { Faker::Dog.name }
-    address { Faker::Providers.address}
-    email { Faker::Internet.email }
-    city { Faker::Providers.city }
-    state { Faker::Providers.state }
-    zip { Faker::Providers.zip }
-    phone { Faker::Providers.phone_number }
-    email { Faker::Internet.safe_email }
->>>>>>> e81f346e72753b4de07d8e6fdc5dda14f733381d
   end
 end
