b52b5bb42eeb2ebb53cb12306ac9b268b82a748f
Jori Peterson <joripeterson@gmail.com>
2019-09-07 17:48:54 -0600

    Update user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # describe 'validations' do
-  #   it {should validate_presence_of(:name)
-  #   it {should validate_presence_of(:address)}
-  #   it {should validate_presence_of(:city)}
-  #   it {should validate_presence_of(:state)}
-  #   it {should validate_presence_of(:zip)}
-  #   it {should validate_presence_of(:email)}
-  # end
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+  end
 
 	describe 'relationships' do
 		it {should have_many :events}
