77ca2b12aeff3e2612294088e7a6731d4c5f0960
Jori Peterson <joripeterson@gmail.com>
2019-09-07 12:21:00 -0600

    Remove class method from user show

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,6 @@ class Event < ApplicationRecord
   validates_presence_of :time
   validates_presence_of :address
   validates_presence_of :user_id
-  # validates_presence_of :event_type
 
   belongs_to :user
   has_many :event_attendees
