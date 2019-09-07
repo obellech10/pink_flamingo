d1885f4799fc16e004064f5241f9e49ff6436425
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-07 07:31:29 -0600

    Adds volunteer button

--- spec/features/user/user_profile_spec.rb
+++ spec/features/user/user_profile_spec.rb
@@ -62,6 +62,8 @@ describe "As a logged in User on my profile page" do
       expect(page.all(".user-phone").first).to have_content(@phone_number)
       expect(page.all(".user-email").first).to have_content(@email)
 
+      expect(page).to have_button("Volunteer to Host an Event")
+
       OmniAuth.config.mock_auth[:google] = nil
     end
   end
