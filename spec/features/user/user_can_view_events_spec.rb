1f5755b9e59b69e97deafe20bbb4063599d63f85
Jori Peterson <joripeterson@gmail.com>
2019-09-09 13:14:22 -0600

    Update test with radius and accurate address

--- spec/features/user/user_can_view_events_spec.rb
+++ spec/features/user/user_can_view_events_spec.rb
@@ -5,20 +5,20 @@ describe "As a user on the Events Index page" do
     it "I see the details of that event, the option to RSVP, and a map" do
       stub_omniauth
 
-      user = create(:user)
-      event = create(:event, user_id: user.id, address: "2795 Speer Blvd")
+      user = User.create!(name: 'Jules Doe', address: "123 Main St", city: "Denver", state: "CO", zip: 80203, email: "joe@example.com")
+      event = user.events.create!(title: "Football Watch Party", date: "9-13-19", time: "8:00pm", address: "123 Main St", food: true, booze: true, radius: 2.0)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit events_path
 
-      expect(page).to have_content("Its my Partaaaaay!")
+      expect(page).to have_content("Football Watch Party")
 
-      click_on "Its my Partaaaaay!"
+      click_on "Football Watch Party"
 
       expect(current_path).to eq(event_path(event))
 
-      expect(page).to have_content("Its my Partaaaaay!")
+      expect(page).to have_content("Football Watch Party")
 
       expect(page).to have_button("RSVP")
 
