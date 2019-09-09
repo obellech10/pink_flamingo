b27a664b539e8040e2e76616da263b2614888435
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-08 18:24:15 -0600

    fixes test

--- spec/features/user/user_can_see_events_with_range_spec.rb
+++ spec/features/user/user_can_see_events_with_range_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "As as a user on my events show page" do
   before :each do
-    # @patty = create(:user, name: "Patty McParty", address: "5083 Verbena St, Denver, CO 80238")
-    # @host1 = create(:user, name: "Hostess w Mostest", address: "8393 Beekman Pl, Denver, CO 80216")
-    # @funevent = create(:event, title: "Deb's not invited", user: @host1, radius: 0.25, restrictions: "Deb", address: "8393 Beekman Pl, Denver, CO 80216")
+    @patty = create(:user, name: "Patty McParty", address: "5083 Verbena St, Denver, CO 80238")
+    @host1 = create(:user, name: "Hostess w Mostest", address: "8393 Beekman Pl, Denver, CO 80216")
+    @funevent = create(:event, title: "Deb's not invited", user: @host1, radius: 0.25, restrictions: "Deb", address: "8393 Beekman Pl, Denver, CO 80216")
 
     @deb = create(:user, name: "Debbie F. Downer", address: "5084 Trenton St, Denver, CO 80238")
     @host2 = create(:user, name: "Nice Guy", address: "5076 Trenton St, Denver, CO 80238")
