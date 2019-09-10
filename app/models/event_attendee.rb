5cd05ff0e0c08ea5d86ca1f9b104e5781590612b
Jori Peterson <joripeterson@gmail.com>
2019-09-10 15:33:09 -0600

    Add attendees method for events
    Co-Authored-By: Mills Provosty <millsprovosty@gmail>

--- app/models/event_attendee.rb
+++ app/models/event_attendee.rb
@@ -1,6 +1,15 @@
 class EventAttendee < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  validates :number_of_guests, numericality: { greater_than: 0, less_than_or_equal_to: 10 }
 
   enum rsvp: ["Not this time!", "Not sure yet!", "I'll be there!"]
+
+  def self.attendees(event)
+    joins(:user)
+    .where("event_attendees.event_id = ?", "#{event.id}")
+    .select("event_attendees.*")
+    .select("users.name")
+    .group(:id, "users.name")
+  end
 end
