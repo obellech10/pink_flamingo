65282786271f206838acf2d5cb739a4de09543f7
Jori Peterson <joripeterson@gmail.com>
2019-09-10 15:31:57 -0600

    Add validation to number_of_guests

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -2,13 +2,15 @@ class EventAttendeesController < ApplicationController
 
   def create
     @event = Event.find(params[:event_id])
-    @event.event_attendees.new({
+    event_attendee = @event.event_attendees.new({
       user: current_user,
       rsvp: params[:rsvp].to_i,
       number_of_guests: params[:number_of_guests]
       })
     if @event.save
       flash[:success] = "Thank you for your response!"
+    else
+      flash[:error] = event_attendee.errors.full_messages.to_sentence  
     end
     redirect_to event_path(@event)
   end
