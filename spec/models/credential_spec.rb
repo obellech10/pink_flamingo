ad8c8ab2cd8094fa080d930f5dbc261c5bdaa04e
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-12 15:21:32 -0600

    Deletes unused tests

--- spec/models/credential_spec.rb
+++ spec/models/credential_spec.rb
@@ -10,10 +10,4 @@ RSpec.describe Credential, type: :model do
 	describe 'relationships' do
 		it {should belong_to :user}
 	end
-
-  describe "creates user with omniauth" do
-    it "find_or_create_by_omniauth" do
-      
-    end
-  end
 end
