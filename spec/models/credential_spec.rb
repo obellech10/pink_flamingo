54ca55e95b816b5b80f233f88cf724af72bf7c10
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-12 15:02:14 -0600

    Adds spec data

--- spec/models/credential_spec.rb
+++ spec/models/credential_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Credential, type: :model do
 	describe 'relationships' do
 		it {should belong_to :user}
 	end
+
+  describe "creates user with omniauth" do
+    it "find_or_create_by_omniauth" do
+      
+    end
+  end
 end
--- spec/models/event_attendee_spec.rb
+++ spec/models/event_attendee_spec.rb
@@ -5,4 +5,42 @@ RSpec.describe EventAttendee, type: :model do
 		it {should belong_to :event}
 		it {should belong_to :user}
 	end
+
+	describe "#attendees" do
+		it "returns the correct attendees" do
+			host1 = create(:user)
+			host2 = create(:user)
+
+			event1 = create(:event, user: host1)
+			event2 = create(:event, user: host2)
+
+			user1 = create(:user)
+			user2 = create(:user)
+			user3 = create(:user)
+			user4 = create(:user)
+			user5 = create(:user)
+			user6 = create(:user)
+			user7 = create(:user)
+			user8 = create(:user)
+			user9 = create(:user)
+			user10 = create(:user)
+
+			ea1 = create(:event_attendees, number_of_guests: 5, user: user1, event: event1, rsvp: 2 )
+			ea2 = create(:event_attendees, number_of_guests: 5, user: user2, event: event1, rsvp: 2)
+			ea3 = create(:event_attendees, number_of_guests: 5, user: user3, event: event1, rsvp: 2)
+			ea4 = create(:event_attendees, number_of_guests: 0, user: user4, event: event1, rsvp: 1)
+			ea5 = create(:event_attendees, number_of_guests: 5, user: user5, event: event1, rsvp: 2)
+			ea6 = create(:event_attendees, number_of_guests: 0, user: user6, event: event1, rsvp: 0)
+			ea7 = create(:event_attendees, number_of_guests: 0, user: user7, event: event1, rsvp: 0)
+			ea8 = create(:event_attendees, number_of_guests: 0, user: user8, event: event1, rsvp: 1)
+			ea9 = create(:event_attendees, number_of_guests: 3, user: user9, event: event2, rsvp: 2)
+			ea10 = create(:event_attendees, number_of_guests: 3, user: user10, event: event2, rsvp: 2)
+
+		#ea 1, 2, 3, and 5 are coming with 5 guests each
+
+			num = EventAttendee.attendees(event1)
+
+			expect(num).to eq(20)
+		end
+	end
 end
