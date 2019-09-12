c3a5ee5c0529dcfbf01a97294eedce84d80fd605
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-12 15:15:04 -0600

    Adds test for event attendees

--- spec/models/event_attendee_spec.rb
+++ spec/models/event_attendee_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe EventAttendee, type: :model do
 			event1 = create(:event, user: host1)
 			event2 = create(:event, user: host2)
 
-			user1 = create(:user)
-			user2 = create(:user)
-			user3 = create(:user)
-			user4 = create(:user)
-			user5 = create(:user)
-			user6 = create(:user)
-			user7 = create(:user)
-			user8 = create(:user)
-			user9 = create(:user)
-			user10 = create(:user)
+			user1 = create(:user, name: "Sarah")
+			user2 = create(:user, name: "Jessica")
+			user3 = create(:user, name: "Whitney")
+			user4 = create(:user, name: "Jeff")
+			user5 = create(:user, name: "Nathan")
+			user6 = create(:user, name: "Jake")
+			user7 = create(:user, name: "Violet")
+			user8 = create(:user, name: "Coy")
+			user9 = create(:user, name: "Saige")
+			user10 = create(:user, name: "Lisa")
 
 			ea1 = user1.event_attendees.create(number_of_guests: 5, event: event1, rsvp: 2 )
 			ea2 = user2.event_attendees.create(number_of_guests: 5, event: event1, rsvp: 2)
@@ -36,11 +36,11 @@ RSpec.describe EventAttendee, type: :model do
 			ea9 = user9.event_attendees.create(number_of_guests: 3, event: event2, rsvp: 2)
 			ea10 = user10.event_attendees.create(number_of_guests: 3, event: event2, rsvp: 2)
 
-		#ea 1, 2, 3, and 5 are coming with 5 guests each
+	
+			not_coming = [user9, user10]
+			people = EventAttendee.attendees(event1)
 
-			num = EventAttendee.attendees(event1)
-
-			expect(num).to eq(20)
+			expect(people).to_not eq(not_coming)
 		end
 	end
 end
