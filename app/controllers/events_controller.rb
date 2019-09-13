d1911cb892214b8a0abbba67d250213f5fb522a8
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-09-12 19:20:44 -0600

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,7 +22,7 @@ class EventsController < ApplicationController
   end
 
   def index
-    @event = Event.all
+    @events = Event.all
     render locals: {
       facade: LocalEventsFacade.new
     }
